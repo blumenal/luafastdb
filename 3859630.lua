49c94b8e6837a0d9ec5a1aaca9841331753d497b
blumenal <69910949+blumenal@users.noreply.github.com>
2026-08-28 13:53:33 -0300

    Add files via upload

---
+++ 3859630.lua
@@ -0,0 +1,25 @@
+-- 3859630's Lua and Manifest Created by Hubcap Manifest
+-- METAL GEAR SOLID MASTER COLLECTION Vol.2
+-- Created: August 27, 2026 at 09:09:36 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 0 (3 excluded)
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(3859630) -- METAL GEAR SOLID MASTER COLLECTION Vol.2
+-- MAIN APP DEPOTS
+addappid(3859631, 1, "b830fb84c02195c072dac60e0adc2360d9d66fba057d849cd4637a111554153e") -- Depot 3859631
+setManifestid(3859631, "4392424844331280835", 183866543)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "5753583882400741046", 25674515)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- EXCLUDED DLCS:
+-- UNRELEASED DLCS (COMMENTED OUT)
+-- addappid(4184350) -- DLC 4184350 (unreleased)
+-- addappid(4184390) -- DLC 4184390 (unreleased)
+-- addappid(4184420) -- DLC 4184420 (unreleased)
