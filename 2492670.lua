72f5a620e20266a517706ac4c5210ba64f682e6e
blumenal <69910949+blumenal@users.noreply.github.com>
2026-08-28 13:56:42 -0300

    Add files via upload

---
+++ 2492670.lua
@@ -0,0 +1,31 @@
+-- 2492670's Lua and Manifest Created by Hubcap Manifest
+-- METAL GEAR SOLID 4: Guns of the Patriots - Master Collection Version
+-- Created: August 28, 2026 at 08:49:07 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 5
+-- Total DLCs: 2 (1 excluded)
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(2492670) -- METAL GEAR SOLID 4: Guns of the Patriots - Master Collection Version
+-- MAIN APP DEPOTS
+addappid(2492671, 1, "a74a5c0f9354b87ee132746f97dca2e436e66f48b1e4e1bca974e23415b04471") -- Depot 2492671
+setManifestid(2492671, "1875973235293599215", 24501734674)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "5753583882400741046", 25674515)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+-- METAL GEAR SOLID 4 Guns of the Patriots - Master Collection Version Japanese Language Pack (AppID: 2743890)
+addappid(2743890)
+addappid(2743890, 1, "01eed52ca6695cc38890967aa0a0030b0bec13a0d4a1e2848ebb544cb93d7618") -- METAL GEAR SOLID 4 Guns of the Patriots - Master Collection Version Japanese Language Pack - Depot 2743890
+setManifestid(2743890, "687647022669389448", 14943786962)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3397310) -- METAL GEAR SOLID 4 Guns of the Patriots - Master Collection Version Pre-Order Bonus
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- METAL GEAR SOLID 4 Guns of the Patriots - Master Collection Version International Language Pack (AppID: 2743880) - missing depot keys
+-- addappid(2743880)
